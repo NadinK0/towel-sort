57fec173bfbca736b1aec087cd82616225e3006f
Nadejda Klinskaya <npantelyeva@gmail.com>
2023-08-20 01:09:20 +0300

    feat: fix bugs at task solution

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@
 
 module.exports = function towelSort (matrix) {
   let newArr = [];
+  matrix.forEach((el,index) => el.forEach(function(el1){
+  el.sort((a,b) => a-b); 
+  if(index%2 !== 0){
+    el.reverse();
+  }
+  }));
   matrix.forEach((el) => el.forEach((el1) => newArr.push(el1)));
-  return newArr.sort((a,b) => a-b);
+  return newArr;
 }
